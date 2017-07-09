495764deec15ec16a2069b73d1314e6ce0ccad69
码农不农 <306419886@qq.com>
2017-07-10 01:51:44 +0800

    test apiDoc

--- routes/Tasks.js
+++ routes/Tasks.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Task=require('../models/Task');
 
+/**
+ * @api {get} /:id 根据ID请求信息
+ * @apiName getId
+ * @apiGroup test
+ * @apiParam {Number} id unique
+ * @apiSuccess {Object}
+ * @apiSuccessExample {json} Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *   "content": "This is an example content"
+ * }
+ */
 router.get('/:id?',function(req,res,next){
 
     if(req.params.id){
